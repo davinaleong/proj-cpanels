7de651182ea7bd266339b4c05cdc0bab8efbe625
Personal Profile <davina.leong.sy@gmail.com>
2021-10-24 15:56:08 +0800

    Done with blank page

---
+++ resources/views/blank.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('page-title', 'Blank')
+
+@section('breadcrumb')
+    <li class="breadcrumb-item active" aria-current="page">Blank</li>
+@endsection
+
+@section('content')
+    <p>Here is a starter page.</p>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,80 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ env('APP_NAME', 'CPanels') }}</title>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body id="top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ route('dashboard') }}">{{ env('APP_NAME', 'CPanels') }}</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <form method="POST" action="" class="d-flex w-100">
+                @csrf
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-primary" type="submit">Search <i class="fas fa-search fa-fw"></i></button>
+            </form>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Actions
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li>
+                            <a class="dropdown-item" href="login.html">
+                                <i class="fas fa-sign-out-alt fa-fw"></i> Logout
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="{{ route('dashboard') }}">
+                                <i class="fas fa-file-alt fa-fw"></i> View Logs
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </div>
-    </body>
+    </div>
+</nav>
+
+<aside class="bg-light">
+    <nav class="nav flex-column">
+        <a class="nav-link" href="{{ route('dashboard') }}"><i class="fas fa-file-alt fa-fw"></i> Activity</a>
+        <a class="nav-link" href="#"><i class="fas fa-boxes fa-fw"></i> Projects</a>
+        <a class="nav-link" href="#"><i class="fas fa-server fa-fw"></i> CPanels</a>
+        <a class="nav-link" href="#"><i class="fas fa-cogs fa-fw"></i> Settings</a>
+    </nav>
+</aside>
+
+<main>
+    <div class="container-fluid">
+        <h1>@yield('page-title')</h1>
+
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                @yield('breadcrumb')
+            </ol>
+        </nav>
+
+        @include('components.alert-info')
+        @include('components.alert-danger')
+
+        @yield('content')
+    </div>
+
+    <a id="btn-top" href="#top" class="btn btn-secondary">Top <i class="fas fa-chevron-up"></i></a>
+</main>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+</body>
 </html>
