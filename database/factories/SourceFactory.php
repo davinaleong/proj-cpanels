df8ab805d5350893b9dee2fd4aab3d7d88054895
Personal Profile <davina.leong.sy@gmail.com>
2021-10-25 16:11:11 +0800

    Added source migration, factory and model

---
+++ database/factories/SourceFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Source;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class SourceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Source::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = $this->faker->name();
+        return [
+            'name' => $name,
+            'table' => Str::snake($name)
+        ];
+    }
+}
