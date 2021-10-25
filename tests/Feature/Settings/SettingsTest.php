d2c6ba43bde37667451aeb2fe7b6cd2e7e758790
Personal Profile <davina.leong.sy@gmail.com>
2021-10-25 16:41:27 +0800

    Added settings page test

--- tests/Feature/Settings/SettingsTest.php
+++ tests/Feature/Settings/SettingsTest.php
@@ -1,30 +1,29 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Settings;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use Illuminate\Support\Facades\Hash;
 
-class BlankTest extends TestCase
+class SettingsTest extends TestCase
 {
     use RefreshDatabase;
 
     public function test_guest_gets_redirected()
     {
-        $this->get('/blank')
+        $this->get('/settings')
             ->assertStatus(302)
             ->assertRedirect('/login');
     }
 
-    public function test_user_can_access_page()
+    public function test_admin_can_access_index()
     {
         $user = User::factory()->create();
 
         $this->actingAs($user)
-            ->get('/blank')
+            ->get('/settings')
             ->assertOk();
     }
 }
