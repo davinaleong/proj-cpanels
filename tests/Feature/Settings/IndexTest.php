618b103fcc7bb906b983574c88d98fd55dc02a3a
Personal Profile <davina.leong.sy@gmail.com>
2021-10-25 16:44:59 +0800

    Renamed test

--- tests/Feature/Settings/IndexTest.php
+++ tests/Feature/Settings/IndexTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class SettingsTest extends TestCase
+class IndexTest extends TestCase
 {
     use RefreshDatabase;
 
