a09f016b695127a3465f3ca5a6579339efe227a7
Personal Profile <davina.leong.sy@gmail.com>
2021-10-26 16:46:57 +0800

    Added Project Type model, table and factory

---
+++ app/Models/ProjectType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectType extends Model
+{
+    use HasFactory;
+
+    protected $table = 'project_types';
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function getCreatedAt()
+    {
+        $dbDatetimeFormat = OtherSettings::getDbDatetimeFormat();
+
+        if (filled($this->created_at)) {
+            $datetimeFormat = OtherSettings::getDatetimeFormat();
+
+            $dt = Carbon::createFromFormat($dbDatetimeFormat, $this->created_at);
+            return $dt->format($datetimeFormat);
+        } else {
+            return '';
+        }
+    }
+
+    public function getUpdatedAt()
+    {
+        $dbDatetimeFormat = OtherSettings::getDbDatetimeFormat();
+
+        if (filled($this->updated_at)) {
+            $datetimeFormat = OtherSettings::getDatetimeFormat();
+
+            $dt = Carbon::createFromFormat($dbDatetimeFormat, $this->updated_at);
+            return $dt->format($datetimeFormat);
+        } else {
+            return '';
+        }
+    }
+}
