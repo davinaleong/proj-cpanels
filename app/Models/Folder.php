7f57aabc8ec956d7f7f034485d9bead7abb3454b
Personal Profile <davina.leong.sy@gmail.com>
2021-10-30 01:04:41 +0800

    Setup folder module

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -7,12 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class ProjectType extends Model
+class Folder extends Model
 {
     use HasFactory;
     use SoftDeletes;
-
-    protected $table = 'project_types';
 
     protected $guarded = [
         'id',
