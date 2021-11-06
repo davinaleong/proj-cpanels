743480149ae137cef3c4b5d25e027064c85291a3
Personal Profile <davina.leong.sy@gmail.com>
2021-11-06 11:32:22 +0800

    Edited nav links

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -10,28 +10,6 @@
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-primary" type="submit">Search <i class="fas fa-search fa-fw"></i></button>
             </form>
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Actions
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li>
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">
-                                    <i class="fas fa-sign-out-alt fa-fw"></i> Logout
-                                </a>
-                            </form>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="{{ route('activities.index') }}">
-                                <i class="fas fa-file-alt fa-fw"></i> View Logs
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
         </div>
     </div>
 </nav>
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -4,5 +4,13 @@
         <a class="nav-link" href="#"><i class="fas fa-boxes fa-fw"></i> Projects</a>
         <a class="nav-link" href="#"><i class="fas fa-server fa-fw"></i> CPanels</a>
         <a class="nav-link" href="{{ route('settings.index') }}"><i class="fas fa-cogs fa-fw"></i> Settings</a>
+        <div class="nav-link">
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">
+                    <i class="fas fa-sign-out-alt fa-fw"></i> Logout
+                </a>
+            </form>
+        </div>
     </nav>
 </aside>
