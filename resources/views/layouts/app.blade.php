148989834a08af2913982284c882ca9545a48a24
Personal Profile <davina.leong.sy@gmail.com>
2022-01-22 00:46:15 +0800

    Removed back to top button label

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
         @yield('content')
     </div>
 
-    <a id="btn-top" href="#top" class="btn btn-secondary">Top <i class="fas fa-chevron-up"></i></a>
+    <a id="btn-top" href="#top" class="btn btn-secondary"><i class="fas fa-chevron-up"></i></a>
 </main>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
