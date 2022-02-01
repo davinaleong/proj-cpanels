0cc5435cf167f783543142fc3f7323b0417d8a1f
Personal Profile <davina.leong.sy@gmail.com>
2022-02-01 12:12:57 +0800

    WIP create project page

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Models\DefaultCredential;
 use App\Models\Folder;
 use App\Models\ProjectType;
 use App\Models\Image;
@@ -19,7 +20,17 @@ class ProjectController extends Controller
 
     public function create()
     {
-        //
+        $folder = Folder::where('name', Project::$SUB_FOLDER)->first();
+
+        return view('projects.create', [
+            'projectTypes' => ProjectType::orderBy('name')
+                ->get(),
+            'images' => Image::where('folder_id', $folder->id)
+                ->orderBy('name')
+                ->get(),
+            'oc_default_credentials' => DefaultCredential::getOc(),
+            'wp_default_credentials' => DefaultCredential::getWp()
+        ]);
     }
 
     public function store(Request $request)
--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -2,8 +2,12 @@
 
 namespace Tests\Feature;
 
-use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Activity;
+use App\Models\Project;
+use App\Models\DemoCpanel;
+use App\Models\LiveCpanel;
+use App\Models\Folder;
+use App\Models\User;use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
@@ -11,7 +15,6 @@ class ProjectTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** @group new */
     public function test_guest_gets_redirected_from_index()
     {
         $this->get('/projects')
@@ -19,13 +22,34 @@ class ProjectTest extends TestCase
             ->assertRedirect('/login');
     }
 
-    /** @group new */
     public function test_admin_can_access_index()
     {
         $user = User::factory()->create();
 
         $this->actingAs($user)
             ->get('/projects')
+            ->assertOk();
+    }
+
+    /** @group new */
+    public function test_guest_gets_redirected_from_create()
+    {
+        $this->get('/projects/create')
+            ->assertStatus(302)
+            ->assertRedirect('/login');
+    }
+
+    /** @group new */
+    public function test_admin_can_access_create()
+    {
+        Folder::factory()
+            ->create([
+                'name' => Project::$SUB_FOLDER
+            ]);
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->get('/projects/create')
             ->assertOk();
     }
 }
